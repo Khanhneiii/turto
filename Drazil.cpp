15c3329f677f85a5e9d108bf493dcdf88cf481b3
Khanhneiii <nguyennhakhanh2003@gmail.com>
2021-11-05 14:28:19 +0700

    add nau ne

--- Drazil.cpp
+++ Drazil.cpp
@@ -4,12 +4,13 @@
 int main()
 {
 	int a,b,k,x,y;
-	scanf ("%d%d%d",&x,&y);
+	scanf ("%d%d%d",&x,&y,&k);
 	a=abs(x);
 	b=abs(y);
 	if(k<(a+b)) {printf ("No");} 
 	else if (k==(a+b)) {printf ("Yes");}
 	else if (k>a+b) {x=(k-(a+b)); if (x%2==0) {printf ("Yes");} else {printf("No");};}
+	printf("10;")
 	return 0;
 }
 
